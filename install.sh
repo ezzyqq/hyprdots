a52390c0d6089a03b180fe2fe9a63b9c232fb606
Ezzy <ezzxqq27@gmail.com>
2025-07-07 13:42:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-if [ "$EUID" -eq 0 ]; then
-  echo "Executed as root!"
+set -e
+
+read -rp "This script will rewrite your .bashrc and some other configs, do you really want to continue? (y/N): " confirm
+if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+  echo "Cancelled"
   exit 1
 fi
 
-echo "test"
+if [ "$EUID" -eq 0 ]; then
+  echo "Don't execute this script as root!"
+  exit 1
+fi
+
+TARGET="$HOME"
+cp -r dots/* $TARGET
+
+echo "Installed successfully!"
