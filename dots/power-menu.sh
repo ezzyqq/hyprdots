5849b61d2662cc8953ea2da161e6b01fe1c94b9a
Ezzy <ezzxqq27@gmail.com>
2025-07-07 14:00:08 +0300

    Add files via upload

---
+++ dots/power-menu.sh
@@ -0,0 +1,14 @@
+# Variants
+options="Log Out\nShutdown\nReboot\nSuspend\nHibernate\nCancel"
+
+choice=$(echo -e "$options" | wofi --dmenu --width=250 --height=200 --hide-scroll -p "Power" -i)
+
+case "$choice" in
+  *Log\ Out*) hyprctl dispatch exit ;;
+  *Shutdown*) systemctl poweroff ;;
+  *Reboot*) systemctl reboot ;;
+  *Suspend*) systemctl suspend ;;
+  *Hibernate*) systemctl hibernate ;;
+  *) exit 0 ;;
+esac
+
