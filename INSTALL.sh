8f6be9d3e9777907039847afa6759e8b24870377
Ezzy <ezzxqq27@gmail.com>
2025-07-13 10:18:45 +0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,7 +4,7 @@ set -e
 
 read -rp "This script will rewrite your .bashrc and some other configs, do you really want to continue? (y/N): " confirm
 if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
-  echo "Cancelled"
+  echo "Cancelled by user"
   exit 1
 fi
 
