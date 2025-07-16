3725e3275d49741a4c62705cce75d8b4b9ea1149
Ezzy <ezzxqq27@gmail.com>
2025-07-16 20:19:50 +0300

    Update power-menu.sh

--- dots/.config/hypr/power-menu.sh
+++ dots/.config/hypr/power-menu.sh
@@ -1,7 +1,7 @@
 # Variants
 options="Log Out\nShutdown\nReboot\nSuspend\nHibernate\nCancel"
 
-choice=$(echo -e "$options" | wofi --dmenu --width=250 --height=300 --hide-scroll -p "Power" -i)
+choice=$(echo -e "$options" | wofi --dmenu --width=250 --height=200 --hide-scroll -p "Power" -i)
 
 case "$choice" in
   *Log\ Out*) hyprctl dispatch exit ;;
